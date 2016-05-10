25aa12416ffd8fdc1bad4330503cf89d10754e24
Argo Roots <argo@roots.ee>
2016-05-10 10:35:24 +0300

    Use chokidar.watch instead of continuous scan for all files

--- entu-cms.js
+++ entu-cms.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-var express = require('express')
-var fs      = require('fs')
-var fse     = require('fs-extra')
-var jade    = require('jade')
-var md      = require('markdown-it')()
-var path    = require('path')
-var stylus  = require('stylus')
-var yaml    = require('js-yaml')
-
+var chokidar = require('chokidar')
+var express  = require('express')
+var fs       = require('fs')
+var fse      = require('fs-extra')
+var jade     = require('jade')
+var md       = require('markdown-it')()
+var path     = require('path')
+var stylus   = require('stylus')
+var yaml     = require('js-yaml')
 
 
 // Markdown-it wrapper to handle empty text
@@ -19,6 +19,7 @@ var markdown = function(text) {
         return ''
     }
 }
+
 
 // Returns file path with locale if exists
 var getFilePath = function(dirName, fileName, locale) {
@@ -37,73 +38,101 @@ var getFilePath = function(dirName, fileName, locale) {
     }
 }
 
-// Scans source folder and generates HTMLs
-var worker = function() {
-    htmlFiles = []
-    css = {}
-    fse.walk(appConf.source)
-        .on('data', function (item) {
-            if (item.path.indexOf('/_') > -1) { return }
 
-            for (var l in appConf.locales) {
-                if (!appConf.locales.hasOwnProperty(l)) { continue }
+// Generates HTMLs from template
+var makeHTML = function(fileEvent, filePath) {
+    var folderName = path.dirname(filePath)
+    var fileName = path.basename(filePath)
+    var locales = []
 
-                try {
-                    var jadeFile = getFilePath(item.path, 'index.jade', appConf.locales[l])
-                    if (!jadeFile) { continue }
+    if (fileName.split('.').length > 2) {
+        locales = [fileName.split('.')[1]]
+    } else {
+        locales = appConf.locales
+    }
 
-                    var configFile = getFilePath(item.path, 'config.yaml', appConf.locales[l])
-                    var config = {}
-                    if (configFile) {
-                        config = yaml.safeLoad(fs.readFileSync(configFile, 'utf8'))
-                    }
+    for (var l in locales) {
+        if (!locales.hasOwnProperty(l)) { continue }
 
-                    var dataFile = getFilePath(item.path, 'data.yaml', appConf.locales[l])
-                    var data = {}
-                    if (dataFile) {
-                        data.D = yaml.safeLoad(fs.readFileSync(dataFile, 'utf8'))
-                    }
+        try {
+            // Get Jade template
+            var jadeFile = getFilePath(folderName, 'index.jade', locales[l])
+            if (!jadeFile) { continue }
 
-                    var styleFile = getFilePath(item.path, 'style.styl', appConf.locales[l])
-                    if (styleFile) {
-                        var styl = stylus(fs.readFileSync(styleFile, 'utf8')).set('warn', false).set('compress', !appConf.stylus.pretty)
-                        if (!css[appConf.locales[l]]) { css[appConf.locales[l]] = [] }
-                        css[appConf.locales[l]].push(styl.render())
-                    }
+            // Get and set data for Jade template
+            var data = {}
+            data.G = {}
+            data.G.base = appConf.build_path
+            data.G.language = locales[l]
+            data.G.path = path.dirname(jadeFile).replace(appConf.source, '').substr(1)
+            data.G.data = appConf.data[locales[l]]
+            data.pretty = appConf.jade.pretty
+            data.basedir = appConf.jade.basedir
+            data.md = markdown
 
-                    data.G = {}
-                    data.G.base = appConf.build_path
-                    data.G.language = appConf.locales[l]
-                    data.G.path = path.dirname(jadeFile).replace(appConf.source, '')
-                    data.G.data = appConf.data[appConf.locales[l]]
-                    data.pretty = appConf.jade.pretty
-                    data.basedir = appConf.jade.basedir
-                    data.md = markdown
-
-                    var html = jade.renderFile(jadeFile, data)
-                    var htmlDir = path.dirname(jadeFile.replace(appConf.source, path.join(appConf.build,  appConf.locales[l])))
-                    var htmlFile = path.join(htmlDir, 'index.html')
-
-                    htmlFiles.push(htmlFile)
-                    fse.outputFileSync(htmlFile, html)
-                } catch (e) {
-                    console.error(e.message)
-                }
-            }
-        })
-        .on('end', function () {
-            for (var l in appConf.locales) {
-                if (!appConf.locales.hasOwnProperty(l)) { continue }
-
-                fse.outputFileSync(path.join(appConf.build, appConf.locales[l], 'style.css'), css[appConf.locales[l]].join('\n'))
+            var dataFile = getFilePath(folderName, 'data.yaml', locales[l])
+            if (dataFile) {
+                data.D = yaml.safeLoad(fs.readFileSync(dataFile, 'utf8'))
             }
 
-            setTimeout(worker, (appConf.timeout * 1000))
-        })
-    console.log('Build finished')
+            var html = jade.renderFile(jadeFile, data)
+            var htmlDir = path.dirname(jadeFile.replace(appConf.source, path.join(appConf.build,  locales[l])))
+            var htmlFile = path.join(htmlDir, 'index.html')
 
+            fse.outputFileSync(htmlFile, html)
+
+            console.log(filePath.replace(appConf.source, ''), '>', htmlFile.replace(appConf.build, ''))
+        } catch (e) {
+            console.error(e.message)
+        }
+    }
 }
 
+
+// Generates CSS from stylus
+var makeCSS = function(fileEvent, filePath) {
+    var folderName = path.dirname(filePath)
+    var fileName = path.basename(filePath)
+    var locales = []
+
+    if (fileName.split('.').length > 2) {
+        locales = [fileName.split('.')[1]]
+    } else {
+        locales = appConf.locales
+    }
+
+    for (var l in locales) {
+        if (!locales.hasOwnProperty(l)) { continue }
+
+        try {
+            if (!stylesList[locales[l]]) { stylesList[locales[l]] = {} }
+
+            var styleFile = getFilePath(folderName, 'style.styl', locales[l])
+            if (styleFile) {
+                var styl = stylus(fs.readFileSync(styleFile, 'utf8')).set('warn', false).set('compress', !appConf.stylus.pretty)
+                stylesList[locales[l]][folderName] = styl.render()
+            } else {
+                delete stylesList[locales[l]][folderName]
+            }
+
+            var css = []
+            for (var s in stylesList[locales[l]]) {
+                if (!stylesList[locales[l]].hasOwnProperty(s)) { continue }
+
+                css.push(stylesList[locales[l]][s])
+            }
+
+            var cssDir = path.join(appConf.build, locales[l])
+            var cssFile = path.join(cssDir, 'style.css')
+
+            fse.outputFileSync(cssFile, css.join('\n'))
+
+            console.log(filePath.replace(appConf.source, ''), '>', cssFile.replace(appConf.build, ''))
+        } catch (e) {
+            console.error(e.message)
+        }
+    }
+}
 
 
 // Open config.yaml
@@ -118,6 +147,7 @@ try {
     process.exit(1)
 }
 
+
 // Set config variables
 appConf.locales = appConf.locales || '.'
 appConf.source = appConf.source || path.join(__dirname, 'source')
@@ -125,7 +155,6 @@ appConf.build = appConf.build || path.join(__dirname, 'build')
 appConf.assets = appConf.assets || path.join(__dirname, 'assets')
 appConf.assets_path = appConf.assets_path || '/assets'
 appConf.build_path = appConf.build_path || '/build'
-appConf.timeout = appConf.timeout || 60
 
 if (appConf.source.substr(0, 1) === '.') {
     appConf.source = path.join(path.dirname(appConfFile), appConf.source)
@@ -150,12 +179,11 @@ for (var l in appConf.locales) {
 }
 
 
-//Printout configuration
-var c = {}
-c[appConfFile] = appConf
-console.log()
-console.log()
-console.log(yaml.safeDump(c))
+// Printout configuration
+// var c = {}
+// c[appConfFile] = appConf
+// console.log()
+// console.log(yaml.safeDump(c))
 
 
 // Start server to listen port 4000
@@ -164,11 +192,19 @@ express()
     .use(appConf.assets_path, express.static(appConf.assets))
     .listen(4000, function() {
         console.log()
-        console.log()
         console.log('Server started at http://localhost:4000')
         console.log()
-        console.log()
-
-        // Start scanning source folder and building
-        worker()
     })
+
+
+// Start to watch Jade files
+chokidar.watch(appConf.source + '/**/index*.jade', { ignored: '*/_*' }).on('all', makeHTML)
+
+
+// Start to watch Yaml files
+chokidar.watch(appConf.source + '/**/data*.yaml', { ignored: '*/_*', ignoreInitial: true }).on('all', makeHTML)
+
+
+// Start to watch style files changes
+var stylesList = {}
+chokidar.watch(appConf.source + '/**/style*.styl', { ignored: '*/_*' }).on('all', makeCSS)
