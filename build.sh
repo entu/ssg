3cd835ba9a4ce6c1457468a276fe457175c39ab9
Argo Roots <argo@roots.ee>
2016-07-13 13:45:28 +0300

    Deploy OSX builds in .dmg

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ rm -rf releases
 electron-packager ./ --platform=mas,win32,linux --arch=x64 --out=releases --overwrite --prune --asar
 
 cd releases/Entu\ CMS-mas-x64
-zip -q -r ../Entu-CMS-osx.zip Entu\ CMS.app -x .DS_Store
+electron-installer-dmg --overwrite --out=./ Entu\ CMS.app Entu\ CMS
+mv Entu\ CMS.dmg ../Entu-CMS-osx.dmg
 cd ../..
 
 cd releases/Entu\ CMS-win32-x64
