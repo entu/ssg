32f78ef9cbb6803294f3ed0ade8119dac128b10f
Argo Roots <argo@roots.ee>
2016-08-18 15:35:22 +0300

    0.7.0

---
+++ json2yaml.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+
+
+if (process.argv.length > 2) {
+    fs.writeFileSync(process.argv[2] + '.yaml', yaml.safeDump(require(process.argv[2]), { indent: 4, lineWidth: 999999999 }))
+}
---
+++ yaml2json.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+
+
+if (process.argv.length > 2) {
+    fs.writeFileSync(process.argv[2] + '.json', JSON.stringify(yaml.safeLoad(fs.readFileSync(process.argv[2], 'utf8')), null, 4))
+}
