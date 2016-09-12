b4d5c23c3363c8526a0ab21a1c8cdd121332143e
Argo Roots <argo@roots.ee>
2016-09-12 14:19:03 +0300

    html2jade helper; helper scripts moved to helpers folder;

---
+++ helpers/html2jade.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const html2jade = require('html2jade')
+
+
+if (process.argv.length > 2) {
+    html2jade.convertHtml(fs.readFileSync(process.argv[2], 'utf8'), { nspaces: 4, noemptypipe: true, donotencode: true, bodyless: true }, function (err, jade) {
+        fs.writeFileSync(process.argv[2] + '.jade', jade)
+    })
+}
