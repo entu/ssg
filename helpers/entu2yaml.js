fee50e487e9d16a17bf73ea6e5955e8afcc8ecc2
Argo Roots <argo@roots.ee>
2017-10-26 12:54:35 +0300

    enty2yaml helper to use new API and just save one data.yaml

--- helpers/entu2yaml.js
+++ helpers/entu2yaml.js
@@ -1,145 +1,89 @@
 #!/usr/bin/env node
 
-const entulib = require('entulib')
+'use strict'
+
+const async = require('async')
 const fs = require('fs-extra')
-const klaw = require('klaw')
-const path = require('path')
+const _ = require('lodash')
+const request = require('request')
 const yaml = require('js-yaml')
 
-// Entities fetched from Entu must have 'path' property or they will get discarded.
 
-const ENTU_OPTIONS = {
-    entuUrl: process.env.ENTU_URL,
-    user: process.env.ENTU_USER || '',
-    key: process.env.ENTU_KEY || ''
+const ENTU_DB = process.env.ENTU_DB
+const ENTU_KEY = process.env.ENTU_KEY
+const ENTU_PARENT = process.env.ENTU_PARENT
+const ENTU_TYPE = process.env.ENTU_TYPE
+const DATA_YAML = process.argv[2]
+
+
+const getPropertyValue = property => {
+    if (property.string) { return property.string }
+    if (property.date) { return property.date }
+    if (property.integer) { return property.integer }
+    if (property.boolean) { return property.boolean }
+    if (property.reference) { return property.reference }
+
+    delete property._id
+
+    return property
 }
 
 
-const PARENT_EID = process.env.PARENT_EID
-const E_DEF = process.env.E_DEF
+request({
+    url: 'https://api.entu.ee/auth',
+    method: 'GET',
+    json: true,
+    'auth': {
+        'bearer': ENTU_KEY
+    }
+}, (error, response, body) => {
+    if (error) { console.error(error) }
+    if (response.statusCode !== 200) { console.error(body) }
 
-
-const ITEM_DIR = process.env.ITEM_DIR
-    ? ( path.isAbsolute(process.env.ITEM_DIR)
-            ? process.env.ITEM_DIR
-            : path.join(process.cwd(), process.env.ITEM_DIR)
-      )
-    : false
-
-const ITEM_YAML = process.env.ITEM_YAML ? process.env.ITEM_YAML : 'data.yaml'
-
-// const ITEM_YAML = process.env.ITEM_YAML
-//     ? ( process.env.ITEM_YAML.split(',').map(function(filepath) {
-//             return path.isAbsolute(filepath)
-//                 ? filepath
-//                 : path.join(process.cwd(), filepath)
-//         })
-//       )
-//     : false
-//
-const LIST_YAML = process.env.LIST_YAML
-    ? ( path.isAbsolute(process.env.LIST_YAML)
-            ? process.env.LIST_YAML
-            : path.join(process.cwd(), process.env.LIST_YAML)
-      )
-    : false
-
-const OUT_DIR = process.env.OUT_DIR
-    ? ( path.isAbsolute(process.env.OUT_DIR)
-            ? process.env.OUT_DIR
-            : path.join(process.cwd(), process.env.OUT_DIR)
-        )
-    : false
-
-
-const saveEntity = function(opEntity) {
-    let out_dir = path.join(OUT_DIR, opEntity.get(['properties','path','values',0,'value']))
-
-    if (ITEM_DIR) {
-        klaw(ITEM_DIR)
-            .on('data', function (item) {
-                if (item.stats.isDirectory()) {
-                    return
-                }
-                fs.copy(item.path, path.join(out_dir, path.basename(item.path)))
-            })
-            .on('end', function (err) {
-                if (err) {
-                    console.log('whoa, ERR:',err)
-                    throw err
-                }
-            })
+    let token = _.get(body, [ENTU_DB, 'token'], '')
+    let qs = {
+        limit: 1000
     }
 
-    let out_file = path.join(out_dir, ITEM_YAML)
-    console.log('save ' + opEntity.get(['id']) + ' | ' + opEntity.get(['displayname']) + out_file)
-    let data_y = yaml.safeDump(opEntity.get(), { indent: 4, lineWidth: 999999999 })
-    fs.outputFileSync(out_file, data_y)
-}
+    if (ENTU_TYPE) {
+        qs['_type.string'] = ENTU_TYPE
+    }
+    if (ENTU_PARENT) {
+        qs['_parent.reference'] = ENTU_PARENT
+    }
 
+    request({
+        url: 'https://api.entu.ee/entity',
+        method: 'GET',
+        json: true,
+        'auth': {
+            'bearer': token
+        },
+        qs: qs
+    }, (error, response, body) => {
+        if (error) { console.error(error) }
+        if (response.statusCode !== 200) { console.error(body) }
 
-const saveEntities = function(opEntities) {
-    let entities = []
-    opEntities.forEach(function(opEntity) {
-        entities.push(opEntity.get())
-        if (!opEntity.get(['properties','path','values',0])) {
-            // console.log('skipping ' + opEntity.get(['id']) + ' | ' + opEntity.get(['displayname']))
-            return
+        let data = []
+
+        for (let i = 0; i < body.entities.length; i++) {
+            let entity = {}
+
+            for (var e in body.entities[i]) {
+                if (!body.entities[i].hasOwnProperty(e)) { continue }
+
+                if (e === '_id') {
+                    entity[e] = body.entities[i][e]
+                } else if (body.entities[i][e].length === 1) {
+                    entity[e] = getPropertyValue(body.entities[i][e][0])
+                } else {
+                    entity[e] = _.map(body.entities[i][e], getPropertyValue)
+                }
+            }
+
+            data.push(entity)
         }
-        saveEntity(opEntity)
-        return
+
+        fs.writeFileSync(DATA_YAML, yaml.safeDump(data, { indent: 4, lineWidth: 999999999 }))
     })
-    if (LIST_YAML) {
-        let data_y = yaml.safeDump(entities, { indent: 4, lineWidth: 999999999 })
-        fs.outputFileSync(LIST_YAML, data_y)
-    }
-}
-
-
-var errored = false
-
-
-if (!ITEM_DIR && !LIST_YAML) {
-    console.error('ITEM_DIR or LIST_YAML is required')
-    errored = true
-}
-
-
-if (!ENTU_OPTIONS.entuUrl) {
-    console.error('Missing mandatory ENTU_URL')
-    errored = true
-}
-
-if (!OUT_DIR) {
-    console.error('Missing mandatory OUT_DIR')
-    errored = true
-} else if (!fs.existsSync(OUT_DIR)) {
-    fs.ensureDirSync(OUT_DIR)
-}
-
-if (errored) { process.exit(1) }
-
-if (PARENT_EID) {
-    console.log('get childs')
-    entulib.getChilds(PARENT_EID, E_DEF, ENTU_OPTIONS)
-    .then(function(opEntities) {
-        saveEntities(opEntities.entities)
-    })
-    .catch(function(reason) {
-        console.log('Caching childs of ' + PARENT_EID + ' failed: ', reason)
-        return
-    })
-} else if (E_DEF) {
-    console.log('get entities')
-    entulib.getEntities(E_DEF, null, null, ENTU_OPTIONS)
-    .then(function(opEntities) {
-        saveEntities(opEntities.entities)
-    })
-    .catch(function(reason) {
-        console.log('Caching ' + E_DEF + ' entities failed: ', reason)
-        return
-    })
-} else {
-    console.error('Please set E_DEF and/or PARENT_EID')
-    process.exit(1)
-}
+})
