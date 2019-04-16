116b4f027c5d34a46788cee0a44cc41144889c90
Argo Roots <argo@roots.ee>
2019-04-16 17:10:57 +0300

    Disable logging on each html

--- index.js
+++ index.js
@@ -330,18 +330,10 @@ module.exports = class {
 
         async.parallel({
             template: (callback) => {
-                console.log(`${(new Date()).toISOString()} - getTemplate start`)
-                this.getTemplate(sourcePath, (e, d) => {
-                    console.log(`${(new Date()).toISOString()} - getTemplate end`)
-                    callback(e, d)
-                })
+                this.getTemplate(sourcePath, callback)
             },
             data: (callback) => {
-                console.log(`${(new Date()).toISOString()} - getData start`)
-                this.getData(sourcePath, (e, d) => {
-                    console.log(`${(new Date()).toISOString()} - getData end`)
-                    callback(e, d)
-                })
+                this.getData(sourcePath, callback)
             }
         }, (err, page) => {
             if (err) { return callback(err) }
@@ -384,14 +376,10 @@ module.exports = class {
                         try {
                             let buildFile = path.join(this.buildDir, buildPath, 'index.html')
 
-                            console.log(`${(new Date()).toISOString()} - compiledPug start - ${buildPath.replace(this.buildDir, '')}`)
-
                             const compiledPug = pug.compile(template.template, data)
                             const dependencies = compiledPug.dependencies.concat(data.dependencies)
                             const html = compiledPug(data)
                             const fileContent = minify(html, htmlMinifyConf)
-
-                            console.log(`${(new Date()).toISOString()} - compiledPug end - ${buildPath.replace(this.buildDir, '')}`)
 
                             fs.outputFile(buildFile, fileContent, (err) => {
                                 if (err) { return callback(this.parseErr(err, data.filename)) }
@@ -407,8 +395,6 @@ module.exports = class {
                                     result.dependencies = dependencies.map(v => v.replace(this.sourceDir, ''))
                                 }
                                 outputFiles.push(result)
-
-                                console.log(`${(new Date()).toISOString()} - file write end - ${buildPath.replace(this.buildDir, '')}`)
 
                                 callback(null)
                             })
